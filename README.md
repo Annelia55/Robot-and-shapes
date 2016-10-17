b49ccc34c53f68bd641e2fb92462c72fe56a7660
Anna Bezrukova <annelia_55@mail.ru>
2016-10-17 03:03:45 +0300

    robot ready

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Robot-and-shapes
+[Try it](https://cdn.rawgit.com/Annelia55/Robot-and-shapes/master/index.html)
+![Picture](Picture.png)
--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-/**
- * Created by macbookpro on 16.10.16.
- */
+$(document).ready(function () {
+    $('.flash').click(function () {
+        $('.brain').toggleClass('laser');
+    });
+
+$(".btncolor").click(function(){
+    var red = Math.floor(Math.random() * 255);
+    var green = Math.floor(Math.random() * 255);
+    var blue = Math.floor(Math.random() * 255);
+
+    //Generate an RGBA value from red, green and blue
+    var randomRGBA = 'rgba('+red+','+green+','+blue+',1)';
+
+    // Changes background color to random
+    $("body").css("background", randomRGBA);
+    });
+});
